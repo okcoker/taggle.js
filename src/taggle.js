9e9096362e1788fb17804c348bab67ee3ba62eb3
Sean Coker <seany789@gmail.com>
2016-02-01 23:05:15 -0500

    Bump version to 1.7.0

--- src/taggle.js
+++ src/taggle.js
@@ -1,6 +1,6 @@
 /* !
  * @author Sean Coker <sean@seancoker.com>
- * @version 1.6.2
+ * @version 1.7.0
  * @url http://sean.is/poppin/tags
  * @license MIT
  * @description Taggle is a dependency-less tagging library
