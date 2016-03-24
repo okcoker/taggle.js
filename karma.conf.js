fde8ec693088926882fe61681b875f5b9c167ec0
Sean Coker <seany789@gmail.com>
2016-03-24 03:37:42 -0400

    Add coverage via coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -3,12 +3,22 @@ module.exports = function(config) {
     config.set({
         basePath: '',
         frameworks: ['mocha', 'sinon-chai'],
+        files: [
+            'src/taggle.js',
+            'test/**/*-test.js'
+        ],
+        preprocessors: {
+            'src/taggle.js': ['coverage']
+        },
         webpackMiddleware: {
             noInfo: true
         },
         coverageReporter: {
-            type: 'html',
-            dir: 'coverage/'
+            dir: 'coverage/',
+            reporters: [
+                // { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' }
+            ]
         },
         reporters: ['progress', 'coverage'],
         port: 9876,
